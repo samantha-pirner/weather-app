9d6c104e11147475435c169312f364745afc4fb5
samantha-pirner <59488098+samantha-pirner@users.noreply.github.com>
2021-11-09 13:41:12 -0500

    Updates to JS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -38,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "8e2bfab4d482ab37bf6cedf3444149ff";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+let city = "Toronto";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
