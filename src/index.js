6f43dc388e31b7bcbd288e671802b48e0c655440
samantha-pirner <59488098+samantha-pirner@users.noreply.github.com>
2021-11-09 19:15:34 -0500

    Added search engine

--- src/index.js
+++ src/index.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8e2bfab4d482ab37bf6cedf3444149ff";
-let city = "Toronto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "8e2bfab4d482ab37bf6cedf3444149ff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("new York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
